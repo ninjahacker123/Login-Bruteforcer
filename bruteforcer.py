4c18074654ac30d9ef4bf21cbe2950358dbaeffd
ninjahacker123 <josephmarc2610@gmail.com>
2020-09-11 20:34:48 +0400

    added disclaimer

--- bruteforcer.py
+++ bruteforcer.py
@@ -1,3 +1,20 @@
+"""
+
+	This is a programme intended to bruteforce a login page But I designed it for my login Page at
+	https://github.com/ninjahacker123/Login-page. It takes a wordlist and iterated through every word
+	sending a post request to the site. When it soesent recieve the try again paige it would mean that
+	we have successfull logged in and it prints thhe password!
+
+	written in Python Python 3.7.6
+
+	DO NOT USE FOR ILLEAGLL PURPOUSES I WILL NOT BE RESPONSIBLE
+
+	Hope you enjoy :)
+
+	-Joseph Marc Antony
+
+"""
+
 import requests
 
 url = "http://localhost:3000"
@@ -7,7 +24,7 @@ wordlist = open(file, "r").readlines()
 for word in wordlist:
 	password = word.strip()
 	payload = data={"username": "test@gmail.com", "password": password}
-	
+
 	if "Uh oh!" in str(requests.post(url, data={"username": "test@gmail.com", "password": password}).content):
 		print(f"[-] Incorrect Password: {password}")
 	else:
